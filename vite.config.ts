e09415e61f80b01f4f4a734d071c401a1e91d718
Antoni Morawski <ant.morawski@gmail.com>
2024-02-07 18:59:41 +0100

    chore: set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  dotenv.config({ path: "./ .env.local" });
+
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/tests/setup.ts",
+    },
+  };
+});
